79e362f624422463df1be0244db7f2f2871091f7
tiago-kelis <tiagokelis2017@gmail.com>
2024-08-31 03:02:41 -0300

    net_core-V1.0.0

---
+++ MVC_2024/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using MVC_2024.Models;
+using System.Diagnostics;
+
+namespace MVC_2024.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Demo()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
